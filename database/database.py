d2ed2672f2c9d2a59f2c26496d13cad53d8dde54
Ran <ran@minutemedia.com>
2021-07-11 17:26:19 +0300

    first commit

---
+++ database/database.py
@@ -0,0 +1,22 @@
+import os
+import sqlite3
+
+
+class Database:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    location = current_dir + '/user.db'
+
+    def create_user(self, first_name, last_name, user_name, email, password):
+        connection = sqlite3.connect(self.location)
+        cursor = connection.cursor()
+        q = f"INSERT INTO `user`(`f_name`,`l_name`,`u_name`,`Email`, `password`) VALUES(?,?,?,?,?)", (
+            first_name, last_name, user_name, email, password)
+        cursor.execute(*q)
+        connection.commit()
+
+    def validate_user(self, email, password):
+        connection = sqlite3.connect(self.location)
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT Email, password FROM `user` WHERE `Email` = '{email}' AND `password` = '{password}'")
+        results = cursor.fetchall()
+        return True if len(results) > 0 else False
---
+++ database/scripts/scheme.sql
@@ -0,0 +1,7 @@
+ CREATE TABLE "user" (
+	"f_name"	TEXT,
+	"l_name"	TEXT,
+	"u_name"	TEXT,
+	"Email"	TEXT UNIQUE,
+	"password"	TEXT
+)
