42059d8688d3abc12425daaf88bb672707be819f
Ran <ran@minutemedia.com>
2021-07-18 06:01:15 +0300

    absolute path call when loading json file

--- app/data/common_functions.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-def load_json_file(path):
-    with open(path, 'r') as f:
-        response_payload = json.loads(f.read())
-    return response_payload
--- user_service/data/data_generator.py
+++ user_service/data/data_generator.py
@@ -1,11 +1,11 @@
 from copy import deepcopy
 
-from .common_functions import load_json_file
+from common_functions import load_json_file
 
 
 class DataGenerator:
-    _user_validation_schema = load_json_file('../schemas/user_validation_schema.json')
-    _login_validation_schema = load_json_file('../schemas/login_validation_schema.json')
+    _user_validation_schema = load_json_file('user_validation_schema.json')
+    _login_validation_schema = load_json_file('login_validation_schema.json')
 
     def user_validation_schema(self):
         user_schema = deepcopy(self._user_validation_schema)
