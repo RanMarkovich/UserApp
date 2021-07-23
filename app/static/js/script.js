1dd83bd2649fdc11ee6ea22cf19e39c5d1e1982c
dor <dorchen2020@gmail.com>
2021-07-24 02:33:49 +0300

    improve UI & JS code and add static HTML to Server Side after submit.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,93 +1,53 @@
 
-var errorsAppears = false;
-
-function validateReg() {
-
-    if (errorsAppears) { errorsInit() }
-
-    let fname = document.getElementById('fname').value;
-    let lname = document.getElementById('lname').value;
-    let userName = document.getElementById('uname').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let flag = true;
-
-    if (fname.length != "") showMessage('message1', true);
-    else {
-        showMessage('message1', false);
-        flag = false;
+// handle inputs in Register page.
+function regInputsHandler(){
+    if(event.target.id == 'fname'){
+        if (event.target.value.length > 0) showMessage('message1', true);
+        else showMessage('message1', false);
     }
+    else if(event.target.id == 'lname'){
+        if (event.target.value.length > 0) showMessage('message2', true);
+        else showMessage('message2', false);
+    }
+    else if(event.target.id == 'uname'){
+        if (event.target.value.length >= 2 && event.target.value.length <= 8) showMessage('message3', true);
+        else showMessage('message3', false);
+    }
+    else if(event.target.id == 'email'){
+        if (isValidEmailRegex(event.target.value)) showMessage('message4', true);
+        else showMessage('message4', false);
+    }
+    else if(event.target.id == 'password'){
+        if (event.target.value.length >= 4 && event.target.value.length <= 8) showMessage('message5', true);
+        else showMessage('message5', false);
+    }
+}
 
-    if (lname.length != "") showMessage('message2', true);
-    else {
-        showMessage('message2', false);
-        flag = false;
-    }
-    if (userName.length >= 2 && userName.length <= 8) showMessage('message3', true);
-    else {
-        showMessage('message3', false);
-        flag = false;
-    }
-    if (isValidEmailRegex(email)) showMessage('message4', true);
-    else {
-        showMessage('message4', false);
-        flag = false;
-    }
-    if (password.length >= 4 && password.length <= 8) showMessage('message5', true);
-    else {
-        showMessage('message5', false);
-        flag = false;
-    }
+// handle inputs in Login page.
+function loginInputsHandler() {
 
-    if (flag == true) return true;
-    errorsAppears = true; 
+    if(event.target.id == 'email'){
+        if (isValidEmailRegex(event.target.value)) showMessage('message6', true);
+        else showMessage('message6', false);
+    }
+    else if(event.target.id == 'password'){
+        if (event.target.value.length >= 4 && event.target.value.length <= 8) showMessage('message7', true);
+        else showMessage('message7', false);
+    }
+}
+
+function showMessage(elementId, isValidMessage) {
+    if (isValidMessage) document.getElementById(elementId).innerHTML = '<p class="valid">OK</p>';
+    else document.getElementById(elementId).innerHTML = '<p class="invalid">Invalid</p>';
+}
+
+// When press on submit button check fields in Register or Login.
+// register validMessagesCount = 5 , Login validMessagesCount = 2
+function isValidFields(validMessagesCount) { //
+    let validMessageList = document.getElementsByClassName('valid');
+    if(validMessageList.length == validMessagesCount) return true;
+    document.querySelector('.span').style.visibility ='visible';
     return false;
-}
-
-function validateLogin() {
-    
-    if (errorsAppears) { errorsInit() }
-    
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let flag = true;
-
-    if (isValidEmailRegex(email))
-        document.getElementById('message6').innerHTML += '';
-    else {
-        showMessage('message6', false);
-        flag = false;
-    }
-    if (password.length >= 4 && password.length <= 8)
-        document.getElementById('message7').innerHTML += '';
-    else {
-        showMessage('message7', false);
-        flag = false;
-    }
-   
-    if (flag == true) return true;
-    errorsAppears = true; 
-    return false;
-}
-
-function showMessage(elementId, isValid) {
-    if (isValid)
-        document.getElementById(elementId).innerHTML += '<p class="valid">OK</p>';
-    else
-        document.getElementById(elementId).innerHTML += '<p class="invalid">Invalid</p>';
-}
-
-// if errors already appears clear them.
-function errorsInit() {
-    let validErrorList = document.getElementsByClassName('valid');
-    let invalidErrorList = document.getElementsByClassName('invalid');
-
-    for (const element of validErrorList) {
-        element.innerHTML = "";
-    }
-    for (const element of invalidErrorList) {
-        element.innerHTML = "";
-    }
 }
 
 function isValidEmailRegex(email) {
