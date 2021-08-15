85c863529c75d0aec3a7f567471a5942f0408fa2
Ran <ran@minutemedia.com>
2021-08-15 20:41:05 +0300

    infrastracture created

---
+++ tests/frontend_tests/test_user_app_title.py
@@ -0,0 +1,31 @@
+import os
+
+from pytest import fixture
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+@fixture
+def browser():
+    driver = webdriver.Chrome(os.path.join(os.path.dirname(__file__), 'chromedriver'))
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+@fixture
+def browser_grid():
+    driver = webdriver.Remote(
+        command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME
+    )
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+def test_user_up_title(browser_grid):
+    browser_grid.get('http://user-app:5000/login')
+    act_title = browser_grid.title
+    assert act_title == 'Login Form', f"failed to get expected title, expected: 'Login Form', instead got: '{act_title}'"
